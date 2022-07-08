808d8be9f04cc3eb0f6fc77e2d143535565db7cd
a-kotynski <94733884+a-kotynski@users.noreply.github.com>
2022-07-08 11:12:20 +0200

    Add project files.

---
+++ ObjectInitializers/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ObjectInitializers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Object initializer - a syntax for quickly initializing an object without
+            // the need to call one of its constructors
+
+
+            //with object initalization syntax:
+            var person = new Person
+            {
+                FirstName = "Abe",
+                LastName = "Cede",
+            };
+        }
+    }
+    public class Person
+    {
+        public int Id;
+        public string FirstName;
+        public string LastName;
+        public DateTime Birthdate;
+
+        
+
+        //without object initialization syntax:
+        //public Person(int id)
+        //{
+
+        //}
+        //public Person(int id, string firstName)
+        //{
+
+        //}
+        //public Person(int id, string firstName, string lastName)
+        //{
+
+        //}
+        //public Person(int id, DateTime birthdate)
+        //{
+
+        //}
+    }
+}
